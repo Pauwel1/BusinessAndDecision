f2ec235ff0f3744413ecb58c45696ac1b1262e36
Pauwel De Wilde <pauweldewilde@gmail.com>
2021-10-15 17:41:18 +0200

    ElementTree

--- main.py
+++ main.py
@@ -1,47 +1,16 @@
-import cv2
+import xml.etree.ElementTree as ET 
 
-image = cv2.imread("/Users/paww/Documents/GitHub/BusinessAndDevelopment/data/train/0a2c344efb5dd5b88450eec236a2aa3b_1.tif")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-cv2.imwrite("graybin.jpg", thresh)
+# Pass the path of the xml document 
+tree = ET.parse('/Users/paww/Documents/GitHub/BusinessAndDecision/data/train_xml/0a948131fe85c38152c0b9b22f7c09fc_3.xml') 
 
-(numLabels, labels, stats, centroids) = cv2.connectedComponentsWithStats(thresh, cv2.CV_32S)
+# get the parent tag 
+root = tree.getroot() 
 
-# loop over the number of unique connected component labels
-for i in range(0, numLabels):
-	# if this is the first component then we examine the
-	# *background* (typically we would just ignore this
-	# component in our loop)
-	if i == 0:
-		text = "examining component {}/{} (background)".format(
-			i + 1, numLabels)
-	# otherwise, we are examining an actual connected component
-	else:
-		text = "examining component {}/{}".format( i + 1, numLabels)
-	# print a status message update for the current connected
-	# component
-	print("[INFO] {}".format(text))
-	# extract the connected component statistics and centroid for
-	# the current label
-	x = stats[i, cv2.CC_STAT_LEFT]
-	y = stats[i, cv2.CC_STAT_TOP]
-	w = stats[i, cv2.CC_STAT_WIDTH]
-	h = stats[i, cv2.CC_STAT_HEIGHT]
-	area = stats[i, cv2.CC_STAT_AREA]
-	(cX, cY) = centroids[i]
+# print the root (parent) tag along with its memory location 
+print("The root attributes: ", root) 
 
-# clone our original image (so we can draw on it) and then draw
-# a bounding box surrounding the connected component along with
-# a circle corresponding to the centroid
-output = image.copy()
-cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 3)
-cv2.circle(output, (int(cX), int(cY)), 4, (0, 0, 255), -1)
+# print the attributes of the first tag  
+print("First tag: ", root[0].attrib) 
 
-# construct a mask for the current connected component by
-# finding a pixels in the labels array that have the current
-# connected component ID
-componentMask = (labels == i).astype("uint8") * 255
-# show our output image and connected component mask
-cv2.imshow("Output", output)
-cv2.imshow("Connected Component", componentMask)
-cv2.waitKey(100000)
+# print the text contained within first subtag of the 5th tag from the parent 
+print(root.text) 
